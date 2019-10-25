e091b405dbec5476995a42e654a5b0886640268d
Endovitskaya Marina <m.endovitskaya@mail.msk>
2019-10-25 20:46:52 +0300

    added pages and test_product_page

---
+++ pages/product_page.py
@@ -0,0 +1,8 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+
+class ProductPage(BasePage):
+
+    def add_product_to_basket(self):
+        button = self.browser.find_element_by_css_selector(".btn-add-to-basket")
+        button.click() 
---
+++ test_product_page.py
@@ -0,0 +1,11 @@
+from .pages.product_page import ProductPage
+from .pages.base_page import BasePage
+
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+
+
+def test_guest_can_add_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
