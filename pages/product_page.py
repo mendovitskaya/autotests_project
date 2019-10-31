e8626541769ff3b3d36c6ceb5eaa8fc8d51d0f66
Endovitskaya Marina <m.endovitskaya@mail.msk>
2019-10-31 19:48:58 +0300

    marked xfail

--- pages/product_page.py
+++ pages/product_page.py
@@ -5,8 +5,8 @@ from selenium.webdriver.common.by import By
 class ProductPage(BasePage):
 
     def add_product_to_basket(self):
-        button_add_to_basket = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_BASKET)
-        button_add_to_basket.click()
+        add_to_basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        add_to_basket_button.click()
 
     def should_be_message_about_adding(self):
         assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME), (
@@ -15,7 +15,7 @@ class ProductPage(BasePage):
             "Message about adding is not presented")
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         message = self.browser.find_element(*ProductPageLocators.MESSAGE_ABOUT_ADDING).text
-        assert product_name in message, "No product name in the message"
+        assert product_name == message, "No product name in the message"
 
     def should_be_message_basket_total(self):
         assert self.is_element_present(*ProductPageLocators.MESSAGE_BASKET_TOTAL), (
@@ -24,4 +24,4 @@ class ProductPage(BasePage):
             "Product price is not presented")
         message_basket_total = self.browser.find_element(*ProductPageLocators.MESSAGE_BASKET_TOTAL).text
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
-        assert product_price in message_basket_total, "No product price in the message"
+        assert product_price == message_basket_total, "No product price in the message"
